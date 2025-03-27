c42716170ff84f9380bc0df3b54ee08c26fb605f
zygisGithub <zygisbuzas@gmail.com>
2025-03-27 16:58:30 +0200

    fixed constact form button colour

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -114,7 +114,7 @@ const Contact = () => {
                     </div>
                     <button
                         type="submit"
-                        className="w-full py-2 px-4 bg-blue-600 hover:bg-blue-700 rounded-md text-white font-medium focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        className="w-full py-2 px-4 bg-blue-500 hover:bg-blue-600 rounded-md text-white font-medium focus:outline-none focus:ring-2 focus:ring-blue-500"
                     >
                         Send Message
                     </button>
