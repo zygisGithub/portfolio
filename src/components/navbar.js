f11eaa830d4f31c30e5b98aa5ebaf39688326110
zygisGithub <zygisbuzas@gmail.com>
2025-03-27 16:15:48 +0200

    fixed some desing problems

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,46 +8,53 @@ function Navbar() {
     const getLinkStyle = (path) => {
         const isActive = location.pathname === path;
         return `
-            px-4 py-2 rounded-md transition-all
-            ${
+      px-4 py-2 rounded-md transition-all
+      ${
             isActive
-                ? 'text-blue-400 border border-blue-400 bg-gray-800/40'
-                : 'border border-transparent hover:border-blue-400 hover:bg-gray-800/30'
+                ? "text-blue-400 border border-blue-400 bg-gray-800/40"
+                : "border border-transparent hover:border-blue-400 hover:bg-gray-800/30"
         }
-        `;
+    `;
     };
 
     return (
-        <div className='flex flex-col text-sm sm:text-xl justify-center sm:justify-start gap-4'>
+        <div className="flex flex-col text-sm sm:text-xl justify-center sm:justify-start gap-4">
             <h1
-                className='text-4xl font-bold text-center'
-                style={{ letterSpacing: '5px' }}
+                className="text-4xl font-bold text-center"
+                style={{ letterSpacing: "5px" }}
             >
                 Žygimantas Buzas
             </h1>
 
-            <div className='flex flex-wrap lg:flex-col sm:flex-row md:flex-row justify-around gap-4'>
+            <div className="grid grid-cols-2 md:flex lg:flex md:flex-row lg:flex-col md:justify-center gap-4 justify-items-center">
+
                 <div
-                    onClick={() => nav('/')}
-                    className={`hover:cursor-pointer  text-center ${getLinkStyle('/')}`}
+                    onClick={() => nav("/")}
+                    className={`hover:cursor-pointer text-center ${getLinkStyle("/")}`}
                 >
                     Home
                 </div>
                 <div
-                    onClick={() => nav('/projects')}
-                    className={`hover:cursor-pointer text-center ${getLinkStyle('/projects')}`}
+                    onClick={() => nav("/projects")}
+                    className={`hover:cursor-pointer text-center ${getLinkStyle(
+                        "/projects"
+                    )}`}
                 >
                     Projects
                 </div>
                 <div
-                    onClick={() => nav('/about')}
-                    className={`hover:cursor-pointer text-center ${getLinkStyle('/about')}`}
+                    onClick={() => nav("/about")}
+                    className={`hover:cursor-pointer text-center ${getLinkStyle(
+                        "/about"
+                    )}`}
                 >
                     About Me
                 </div>
                 <div
-                    onClick={() => nav('/contact')}
-                    className={`hover:cursor-pointer text-center ${getLinkStyle('/contact')}`}
+                    onClick={() => nav("/contact")}
+                    className={`hover:cursor-pointer text-center ${getLinkStyle(
+                        "/contact"
+                    )}`}
                 >
                     Contact Me
                 </div>
